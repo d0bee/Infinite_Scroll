99751e1092c1de88d9e9a80441fdf42dec0d26a4
gusdu <gusdud9745@gmail.com>
2023-07-22 21:57:52 +0900

    refactor : code optimization, infinite scroll Constructing infinite scrolling

--- ccgame/app/src/main/java/com/example/ccgame/MainActivity.kt
+++ ccgame/app/src/main/java/com/example/ccgame/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import com.example.ccgame.databinding.ActivityMainBinding
 // MPCHART 사용시 필요한 import문
 import com.github.mikephil.charting.data.*
-// 그외 ex) Color, SetDraw
+// 그외
 import android.graphics.*
 import java.util.*
 
@@ -46,12 +46,14 @@ class MainActivity : AppCompatActivity() {
             highLightColor = Color.TRANSPARENT
         }
 
+        // 좌측 눈금, 박스라인
         binding.chart.axisLeft.run {
             setDrawAxisLine(false)
             setDrawGridLines(false)
             textColor = Color.TRANSPARENT
         }
 
+        // 우측 눈금
         binding.chart.axisRight.run {
             isEnabled = false
         }
@@ -75,98 +77,6 @@ class MainActivity : AppCompatActivity() {
             isHighlightPerDragEnabled = true
             requestDisallowInterceptTouchEvent(true)
             invalidate()
-        }
-    }
-
-    data class CSStock(
-        var createdAt: Long = 0,
-        val open: Float,
-        val close: Float,
-        val shadowHigh: Float,
-        val shadowLow: Float
-    )
-
-    object DataUtil {
-        fun getCSStockData(): List<CSStock> {
-            return listOf(
-                CSStock(
-                    createdAt = 0,
-                    open = 222.8F,
-                    close = 222.9F,
-                    shadowHigh = 224.0F,
-                    shadowLow = 222.2F
-                ),
-                CSStock(
-                    createdAt = 1,
-                    open = 222.0F,
-                    close = 222.2F,
-                    shadowHigh = 222.4F,
-                    shadowLow = 222.0F
-                ),
-                CSStock(
-                    createdAt = 2,
-                    open = 222.2F,
-                    close = 221.9F,
-                    shadowHigh = 222.5F,
-                    shadowLow = 221.5F
-                ),
-                CSStock(
-                    createdAt = 3,
-                    open = 222.4F,
-                    close = 222.3F,
-                    shadowHigh = 223.7F,
-                    shadowLow = 222.1F
-                ),
-                CSStock(
-                    createdAt = 4,
-                    open = 221.6F,
-                    close = 221.9F,
-                    shadowHigh = 221.9F,
-                    shadowLow = 221.5F
-                ),
-                CSStock(
-                    createdAt = 5,
-                    open = 221.8F,
-                    close = 224.9F,
-                    shadowHigh = 225.0F,
-                    shadowLow = 221.0F
-                ),
-                CSStock(
-                    createdAt = 6,
-                    open = 225.0F,
-                    close = 220.2F,
-                    shadowHigh = 225.4F,
-                    shadowLow = 219.2F
-                ),
-                CSStock(
-                    createdAt = 7,
-                    open = 222.2F,
-                    close = 225.9F,
-                    shadowHigh = 227.5F,
-                    shadowLow = 222.2F
-                ),
-                CSStock(
-                    createdAt = 8,
-                    open = 226.0F,
-                    close = 228.1F,
-                    shadowHigh = 228.1F,
-                    shadowLow = 225.1F
-                ),
-                CSStock(
-                    createdAt = 9,
-                    open = 227.6F,
-                    close = 228.9F,
-                    shadowHigh = 230.9F,
-                    shadowLow = 226.5F
-                ),
-                CSStock(
-                    createdAt = 10,
-                    open = 228.6F,
-                    close = 228.6F,
-                    shadowHigh = 230.9F,
-                    shadowLow = 228.0F
-                )
-            )
         }
     }
 }
